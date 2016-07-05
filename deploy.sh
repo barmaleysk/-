bc60e72c91d6b31ae967b87ed6d38aa1e199d8ff
Nickolay Yegorov <nickolayyegorov@gmail.com>
2016-07-05 11:12:03 +0300

    upd

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-ssh -i "./botx.pem" ubuntu@ec2-52-34-35-240.us-west-2.compute.amazonaws.com << EOF
-cd ~/marketbot && git pull origin && ~/marketbot/stop.sh && ~/marketbot/start.sh
-EOF
+ssh -i "./botx.pem" ubuntu@ec2-52-34-35-240.us-west-2.compute.amazonaws.com  << EOF
+cd ~/marketbot && git pull origin && source venv/bin/activate && pip install -r requirements.txt && echo "`whoami`" &&  (exec "./stop.sh") && (exec "./start.sh") && echo '[OK]'
+EOF 
