55e23423773f01b5c5e17bafe4914b2a086e2eb5
Nickolay Yegorov <nickolayyegorov@gmail.com>
2016-07-05 11:01:35 +0300

    upd

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ssh -i "./botx.pem" ubuntu@ec2-52-34-35-240.us-west-2.compute.amazonaws.com << EOF
-cd ~/botx && git pull origin && ~/botx/stop.sh && ~/botx/start.sh
+cd ~/marketbot && git pull origin && ~/marketbot/stop.sh && ~/marketbot/start.sh
 EOF
