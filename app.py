3c80081dd51661250eeec3e645037754e6bd148d
Nickolay Yegorov <nickolayyegorov@gmail.com>
2016-07-21 16:03:04 +0300

    fix

--- app.py
+++ app.py
@@ -1072,7 +1072,7 @@ class MarketBot(object):
 
     def start(self):
         self._init_bot(threaded=True)
-        Process(target=self._start_bot).start()
+        self._start_bot()#.start()
 
 
 class MasterBot(MarketBot):
