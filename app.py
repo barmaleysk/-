a3ed9e3d41f8a25e7d79487f3d1e09528fa4ec03
Nickolay Yegorov <nickolayyegorov@gmail.com>
2016-08-11 16:53:44 +0300

    hotfix

--- app.py
+++ app.py
@@ -727,6 +727,7 @@ class WebhookProcessor(Singleton):
     WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
     WEBHOOK_SSL_CERT = '/home/ubuntu/webhook_cert.pem'  # Path to the ssl certificate
     WEBHOOK_SSL_PRIV = '/home/ubuntu/webhook_pkey.pem'  # Path to the ssl private key
+    WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 
     app = flask.Flask(__name__)
 
