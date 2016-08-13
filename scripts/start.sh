417276bc8c93c959105e44727da3bdfb8316dfbf
Nickolay Yegorov <nickolayyegorov@gmail.com>
2016-08-13 03:35:22 +0300

    port

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /home/ubuntu/marketbot/sendgrid.env
 source /home/ubuntu/marketbot/venv/bin/activate
-python /home/ubunut/marketbot/webhook_listener.py 8433 &
+python /home/ubunut/marketbot/webhook_listener.py 8443 &
 python /home/ubuntu/marketbot/bot_worker.py & 
 echo "`ps aux |grep py`" 
